a5b7478945a26c09c82ebca30b14d1429c5ff58b
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-13 15:24:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,8 +1,11 @@
 # code here!
 class School 
- attr_accessor :roster 
+ attr_accessor :name, :roster 
+  
   def initialize (name)
     @name = name
+    @roster = {}
   end 
-  
+ def add_student (student_name, grade)
+   roster[grade] 
 end 
