725dd30d4cc58217469f39fd299dd3d103ac91f8
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-13 15:26:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,12 @@ class School
     @roster = {}
   end 
  def add_student (student_name, grade)
+   if !roster.include?(grade)
    roster[grade] = []
    roster[grade] << student_name
+ else 
+   roster[grade] << student_name
  end 
+end 
  
 end 
