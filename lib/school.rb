f735f7ae4a015a8b653c2f8fcdf702e12a69265d
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-13 16:05:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,6 +24,7 @@ end
     roster.collect do |grade, students|
       {grade => roster[grade].sort!}
     end
+    puts roster
     # roster = roster.first
     # roster.sort_by {|grade, v| grade}
  end
